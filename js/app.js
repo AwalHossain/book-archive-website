687a71839f39ae5a23b19a92451ef2adbe6b69f1
AwalHossain <waqin21@gmail.com>
2021-09-02 01:02:44 +0600

    Most of the basic part are done. Now, all i need to modifie the code and throw some error

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Selector
 const input = document.getElementById('input');
 const searchBtn = document.getElementById('search')
-
+const column = document.querySelector('.column')
 
 searchBtn.addEventListener('click', ()=>{
     const searchText = input.value
@@ -16,6 +16,9 @@ const fetchApi = (url) =>{
     fetch(url)
     .then(response => response.json())
     .then(data => showBooks(data))
+    .catch(error => {
+        console.log(error, "not found");
+    })
    
 }
 
@@ -23,8 +26,32 @@ const fetchApi = (url) =>{
 const showBooks = (data) =>{
     const bookDetails = data.docs;
     bookDetails.forEach(book =>{
-        console.log(book.title);
-        
+        console.log(book);
+        column.innerHTML += `
+        <div class="col ">
+        <div class="card">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid text-center" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Book-Name: ${book.title}</h5>
+          <h6>Author: ${book.author_name}</h6>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+        </div>
+        `
+        // const div = document.createElement('div');
+        // div.classList.add( "col-4","g-3", "text-center", "border" ,"border-2")
+        //   div.innerHTML =`
+        //   <img width="350px" class="img-fluid" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"?alt='not available'>
+        //   <h3>Book-Name: ${book.title}</h3>
+        //   <h4>Author: ${book.author_name}</h4>
+        // <p></p>
+        //   `
+        //   console.log("bold");
+     
+    
+        // card.appendChild(div)
+
     })
 }
 
